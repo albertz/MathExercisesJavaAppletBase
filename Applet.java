5d65766a214e9d37ec1b232bbe06b9b54e7d0d3d
zeyer <zeyer@688fd007-192f-0410-aed6-f784cf09bbae>
2007-05-14 17:39:41 +0000

    erstes Applet erstellt
    git-svn-id: svn+ssh://demeter.matha.rwth-aachen.de/home/Projekte/ilias/svn@10 688fd007-192f-0410-aed6-f784cf09bbae
    
    new applet: M3_01_04

---
+++ Applet.java
@@ -0,0 +1,14 @@
+package applets.M3_01_04;
+
+import net.mumie.mathletfactory.appletskeleton.g2d.SingleG2DCanvasApplet;
+
+public class Applet extends SingleG2DCanvasApplet {
+
+	public void init() {
+		super.init();
+		
+		
+	}
+	
+	
+}
