b1559c81f37b2aa5cb45b89f68c3185f63c8be92
zeyer <zeyer@688fd007-192f-0410-aed6-f784cf09bbae>
2010-10-19 00:50:03 +0000

    simple normalisation ready
    git-svn-id: svn+ssh://demeter.matha.rwth-aachen.de/home/Projekte/ilias/svn@3539 688fd007-192f-0410-aed6-f784cf09bbae

--- EquationSystem.java
+++ EquationSystem.java
@@ -1,11 +1,17 @@
 package applets.Termumformungen$in$der$Technik_01_URI;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import applets.Termumformungen$in$der$Technik_01_URI.EquationSystem.Equation.FracSum.Frac;
+import applets.Termumformungen$in$der$Technik_01_URI.EquationSystem.Equation.FracSum.Frac.Sum;
+import applets.Termumformungen$in$der$Technik_01_URI.EquationSystem.Equation.FracSum.Frac.Sum.Prod;
 
 public class EquationSystem {
 
@@ -73,32 +79,70 @@ public class EquationSystem {
 		@Override public String toString() { return Utils.concat(facs, " "); }
 	}
 	
-	static class VariableSymbol {
+	static class VariableSymbol implements Comparable<VariableSymbol> {
 		Unit unit;
 		String name;
 		VariableSymbol(String unit) { this.unit = new Unit(unit); }
 		VariableSymbol(String name, String unit) { this.name = name; this.unit = new Unit(unit); }
+		public int compareTo(VariableSymbol o) { if(this == o) return 0; return name.compareTo(o.name); }		
 	}
 	
 	Map<String, VariableSymbol> variableSymbols = new HashMap<String, VariableSymbol>();	
 	void registerVariableSymbol(VariableSymbol var) { variableSymbols.put(var.name, var); }	
 	void registerVariableSymbol(String name, String unit) { registerVariableSymbol(new VariableSymbol(name, unit)); }
 	
-	static class Equation {
-		static class FracSum {
-			static class Frac {
-				static class Sum {		
-					static class Prod {
+	static class Equation implements Comparable<Equation> {
+		static class FracSum implements Comparable<FracSum> {
+			static class Frac implements Comparable<Frac> {
+				static class Sum implements Comparable<Sum> {
+					static class Prod implements Comparable<Prod> {
 						int fac = 1;
-						static class Pot {
+						static class Pot implements Comparable<Pot> {
 							VariableSymbol sym;
 							int pot = 1;
 							@Override public String toString() { return sym.name + ((pot != 1) ? ("^" + pot) : ""); }		
 							Pot(VariableSymbol s) { sym = s; }
+							Pot(VariableSymbol s, int p) { sym = s; pot = p; }
+							public int compareTo(Pot o) { if(sym != o.sym) return sym.compareTo(o.sym); return pot - o.pot; }
+							@Override public int hashCode() {
+								int result = 1;
+								result = 31 * result + pot;
+								result = 31 * result + ((sym == null) ? 0 : sym.hashCode());
+								return result;
+							}
+							@Override public boolean equals(Object obj) {
+								if (this == obj) return true;
+								if (obj == null) return false;
+								if (getClass() != obj.getClass()) return false;
+								Pot other = (Pot) obj;
+								if (pot != other.pot) return false;
+								return sym == other.sym;
+							}
 						}						
 						List<Pot> facs = new LinkedList<Pot>();
-						@Override public String toString() { return ((fac != 1) ? "" + fac + " ∙ " : "") + Utils.concat(facs, " ∙ "); }						
+						public int compareTo(Prod o) {
+							int r = Utils.<Pot>orderOnCollection().compare(facs, o.facs);
+							if(r != 0) return r;
+							return fac - o.fac;
+						}
+						@Override public String toString() { return ((fac != 1) ? "" + fac + " ∙ " : "") + Utils.concat(facs, " ∙ "); }
+						Prod normalize() {
+							Prod prod = new Prod();
+							prod.fac = fac;
+							Map<VariableSymbol,Integer> vars = new HashMap<VariableSymbol,Integer>();
+							for(Pot p : facs) {
+								if(!vars.containsKey(p.sym)) vars.put(p.sym, 0);
+								vars.put(p.sym, vars.get(p.sym) + p.pot);
+							}
+							for(VariableSymbol sym : vars.keySet()) {
+								if(vars.get(sym) != 0)
+									prod.facs.add(new Pot(sym, vars.get(sym)));
+							}
+							return prod;
+						}
+						Prod minusOne() { return new Prod(-fac, facs); }
 						Prod() {}
+						Prod(int fac, List<Pot> facs) { this.fac = fac; this.facs = facs; }
 						Prod(Utils.OperatorTree ot, Map<String,VariableSymbol> vars) throws ParseError { parse(ot, vars); }
 						void parse(Utils.OperatorTree ot, Map<String,VariableSymbol> vars) throws ParseError {
 							if(ot.canBeInterpretedAsUnaryPrefixed() && ot.op.equals("-")) {
@@ -133,7 +177,34 @@ public class EquationSystem {
 					}
 					List<Prod> entries = new LinkedList<Prod>();
 					@Override public String toString() { return ((entries.size() > 1) ? "(" : "") + Utils.concat(entries, " + ") + ((entries.size() > 1) ? ")" : ""); }
+					public int compareTo(Sum o) { return Utils.<Prod>orderOnCollection().compare(entries, o.entries); }
 					boolean isEmpty() { return entries.isEmpty(); }
+					Sum normalize() {
+						Sum sum = new Sum();
+						List<Prod> newEntries = new LinkedList<Prod>();
+						for(Prod prod : entries)
+							newEntries.add(prod.normalize());
+						Collections.sort(newEntries);
+						Prod lastProd = null;
+						for(Prod prod : newEntries) {
+							if(lastProd != null && lastProd.facs.equals(prod.facs))
+								lastProd.fac += prod.fac;
+							else {
+								lastProd = prod;
+								sum.entries.add(lastProd);
+							}
+						}
+						for(Iterator<Prod> pit = sum.entries.iterator(); pit.hasNext();) {
+							if(pit.next().fac == 0)
+								pit.remove();
+						}
+						return sum;
+					}
+					Sum minusOne() {
+						Sum sum = new Sum();
+						for(Prod prod : entries) sum.entries.add(prod.minusOne());
+						return sum;
+					}
 					Sum() {}
 					Sum(Utils.OperatorTree ot, Map<String,VariableSymbol> vars) throws ParseError {
 						if(ot.op.equals("+") || ot.entities.size() <= 1) {
@@ -146,7 +217,25 @@ public class EquationSystem {
 				}
 				Sum numerator = new Sum(), denominator = null;
 				@Override public String toString() { return numerator.toString() + ((denominator != null) ? " / " + denominator.toString() : ""); }
+				boolean hasEqualDenominatorAs(Frac f) {
+					if(denominator == null && f.denominator == null) return true;
+					if(denominator != null && f.denominator == null) return false;
+					if(denominator == null && f.denominator != null) return false;
+					return denominator.equals(f.denominator);
+				}
+				public int compareTo(Frac o) {
+					if(denominator != null && o.denominator == null) return 1;
+					if(denominator == null && o.denominator != null) return -1;
+					if(denominator != null && o.denominator != null) {
+						int r = denominator.compareTo(o.denominator);
+						if(r != 0) return r;
+					}
+					return numerator.compareTo(o.numerator);					
+				}
+				Frac normalize() { return new Frac(numerator.normalize(), denominator.normalize()); }
+				Frac minusOne() { return new Frac(numerator.minusOne(), denominator); }
 				Frac() {}
+				Frac(Sum numerator, Sum denominator) { this.numerator = numerator; this.denominator = denominator; }
 				Frac(Utils.OperatorTree ot, Map<String,VariableSymbol> vars) throws ParseError {
 					if(!ot.op.equals("/"))
 						numerator = new Sum(ot, vars);
@@ -159,6 +248,34 @@ public class EquationSystem {
 			}
 			List<Frac> entries = new LinkedList<Frac>();
 			@Override public String toString() { return Utils.concat(entries, " + "); }
+			public int compareTo(FracSum o) { return Utils.<Frac>orderOnCollection().compare(entries, o.entries); }
+			FracSum normalize() {
+				List<Frac> newEntries = new LinkedList<Frac>();
+				for(Frac f : entries) newEntries.add(f.normalize());
+				Collections.sort(newEntries);
+				FracSum sum = new FracSum();
+				Frac lastFrac = null;
+				for(Frac f : newEntries) {
+					if(lastFrac != null && lastFrac.hasEqualDenominatorAs(f)) {
+						lastFrac.numerator.entries.addAll(f.numerator.entries);
+						lastFrac.numerator = lastFrac.numerator.normalize();
+					}
+					else {
+						lastFrac = f;
+						sum.entries.add(lastFrac);
+					}
+				}
+				for(Iterator<Frac> fit = sum.entries.iterator(); fit.hasNext();) {
+					if(fit.next().numerator.isEmpty())
+						fit.remove();
+				}
+				return sum;
+			}
+			FracSum minusOne() {
+				FracSum sum = new FracSum();
+				for(Frac f : entries) sum.entries.add(f.minusOne());
+				return sum;
+			}
 			FracSum() {}
 			FracSum(Utils.OperatorTree ot, Map<String,VariableSymbol> vars) throws ParseError {
 				if(ot.op.equals("+") || ot.entities.size() <= 1) {
@@ -171,6 +288,17 @@ public class EquationSystem {
 		}
 		FracSum left = new FracSum(), right = new FracSum();		
 		@Override public String toString() { return left.toString() + " = " + right.toString(); }
+		public int compareTo(Equation o) {
+			int r = left.compareTo(o.left); if(r != 0) return r;
+			return right.compareTo(o.right);
+		}
+		Equation normalize() {
+			Equation eq = new Equation();
+			eq.left.entries.addAll(left.entries);
+			eq.left.entries.addAll(right.minusOne().entries);
+			eq.left = eq.left.normalize();
+			return eq;
+		}
 		Equation() {}
 		Equation(Utils.OperatorTree ot, Map<String,VariableSymbol> vars) throws ParseError {
 			if(ot.entities.size() == 0) throw new ParseError("Please give me an equation.");
