3883a178f22aaa55087b74a8842c4226ed28ce69
zeyer <zeyer@688fd007-192f-0410-aed6-f784cf09bbae>
2010-10-29 00:39:18 +0000

    export SimpleLabel
    git-svn-id: svn+ssh://demeter.matha.rwth-aachen.de/home/Projekte/ilias/svn@3587 688fd007-192f-0410-aed6-f784cf09bbae

---
+++ SimpleLabel.java
@@ -0,0 +1,66 @@
+package applets.Termumformungen$in$der$Technik_01_URI;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.geom.Rectangle2D;
+
+import javax.swing.JComponent;
+
+class SimpleLabel extends JComponent {
+	private static final long serialVersionUID = 1L;
+	
+	String text = "";
+	Integer fixedWidth = null;
+	
+	SimpleLabel() {}
+	SimpleLabel(String text) { this.text = text; setFont(Applet.defaultFont); }
+	SimpleLabel(String text, int fixedWidth) { this.text = text; this.fixedWidth = fixedWidth; setFont(Applet.defaultFont); }
+	SimpleLabel(String text, int fixedWidth, Font font) { this.text = text; this.fixedWidth = fixedWidth; setFont(font); }
+	
+	private abstract class WalkStringCallback {
+		abstract void exec(String s, int x, int y);
+	}	
+	private Dimension walkString(WalkStringCallback callback) {
+		Point cur = new Point();
+		Dimension bounds = new Dimension();
+		final int lineSpace = 5;
+		FontMetrics metrics = getFontMetrics(getFont());
+		for(String s : Utils.tokenizeString(text)) {
+			if(s.equals("\n")) {
+				cur.x = 0;
+				cur.y += metrics.getHeight() + lineSpace;
+				continue;
+			}
+			Rectangle2D rect = metrics.getStringBounds(s, getGraphics());
+			if(fixedWidth != null) {
+				if(cur.x > 0 && cur.x + rect.getWidth() > fixedWidth) {
+					cur.x = 0;
+					cur.y += metrics.getHeight() + lineSpace;				
+				}
+			}
+			if(callback != null)
+				callback.exec(s, cur.x, cur.y + metrics.getLeading() + metrics.getMaxAscent());
+			cur.x += rect.getWidth();
+			bounds.width = Math.max(bounds.width, cur.x);
+		}
+		bounds.height = cur.y + metrics.getHeight();
+		return bounds;
+	}
+	
+	@Override public void paint(final Graphics g) {
+		super.paint(g);
+		g.setFont(getFont());
+		walkString(new WalkStringCallback() {
+			@Override void exec(String s, int x, int y) {
+				g.drawString(s, x, y);
+			}
+		});
+	}
+	
+	@Override public Dimension getPreferredSize() {
+		return walkString(null);
+	}
+}
--- VTLabel.java
+++ VTLabel.java
@@ -5,17 +5,7 @@ package applets.Termumformungen$in$der$Technik_01_URI;
 
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.Graphics;
-import java.awt.Point;
-import java.awt.geom.Rectangle2D;
-import java.util.StringTokenizer;
-
-import javax.swing.JComponent;
-import javax.swing.JLabel;
-import javax.swing.JTextArea;
 
 
 
@@ -53,63 +43,18 @@ public class VTLabel extends VisualThing {
 	private String text;
 	private Font font = null;
 	private Color color = Color.black;
-	private Label label = null;
+	private SimpleLabel label = null;
 	private Integer fixedWidth = null;
 
 	public String getName() { return name; }
 	
-	class Label extends JComponent {
-		private static final long serialVersionUID = 1L;
-		abstract class WalkStringCallback {
-			abstract void exec(String s, int x, int y);
-		}
-		
-		Dimension walkString(WalkStringCallback callback) {
-			Point cur = new Point();
-			Dimension bounds = new Dimension();
-			final int lineSpace = 5;
-			FontMetrics metrics = getFontMetrics(font);
-			for(String s : Utils.tokenizeString(text)) {
-				if(s.equals("\n")) {
-					cur.x = 0;
-					cur.y += metrics.getHeight() + lineSpace;
-					continue;
-				}
-				Rectangle2D rect = metrics.getStringBounds(s, getGraphics());
-				if(fixedWidth != null) {
-					if(cur.x > 0 && cur.x + rect.getWidth() > fixedWidth) {
-						cur.x = 0;
-						cur.y += metrics.getHeight() + lineSpace;				
-					}
-				}
-				if(callback != null)
-					callback.exec(s, cur.x, cur.y + metrics.getLeading() + metrics.getMaxAscent());
-				cur.x += rect.getWidth();
-				bounds.width = Math.max(bounds.width, cur.x);
-			}
-			bounds.height = cur.y + metrics.getHeight();
-			return bounds;
-		}
-		
-		@Override public void paint(final Graphics g) {
-			super.paint(g);
-			g.setFont(font);
-			walkString(new WalkStringCallback() {
-				@Override void exec(String s, int x, int y) {
-					g.drawString(s, x, y);
-				}
-			});
-		}
-		
-		@Override public Dimension getPreferredSize() {
-			return walkString(null);
-		}
-	}
-	
+
 	public void setText(String text) {
 		this.text = text;
-		if(label != null)
+		if(label != null) {
+			label.text = text;
 			label.repaint(); // to recalculate the size
+		}
 	}
 	
 	public String getText() {
@@ -152,12 +97,14 @@ public class VTLabel extends VisualThing {
 	
 	public Component getComponent() {
 		if (label == null) {
-			label = new Label();
+			label = new SimpleLabel();
 			if (name != null)
 				label.setName(name);
+			label.text = text;
 			label.setFont(font);
 			label.setForeground(color);
 			label.setOpaque(false);
+			label.fixedWidth = fixedWidth;
 			//label.setBackground(Color.cyan);
 		}
 		return label;
