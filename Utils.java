0c4728896ea6a941ecd361af444736fc75465b6f
zeyer <zeyer@688fd007-192f-0410-aed6-f784cf09bbae>
2011-02-07 12:08:41 +0000

    impl coll.contains
    git-svn-id: svn+ssh://demeter.matha.rwth-aachen.de/home/Projekte/ilias/svn@3725 688fd007-192f-0410-aed6-f784cf09bbae

--- Utils.java
+++ Utils.java
@@ -86,7 +86,11 @@ public class Utils {
 		public boolean add(E e) { throw new UnsupportedOperationException(); }
 		public boolean addAll(Collection<? extends E> c) { throw new UnsupportedOperationException(); }
 		public void clear() { throw new UnsupportedOperationException(); }
-		public boolean contains(Object o) { throw new UnsupportedOperationException(); }
+		public boolean contains(Object o) {
+			for(Iterator<E> i = iterator(); i.hasNext(); )
+				if(i.next().equals(o)) return true;
+			return false;
+		}
 		public boolean containsAll(Collection<?> c) { throw new UnsupportedOperationException(); }
 		public boolean remove(Object o) { throw new UnsupportedOperationException(); }
 		public boolean removeAll(Collection<?> c) { throw new UnsupportedOperationException(); }
