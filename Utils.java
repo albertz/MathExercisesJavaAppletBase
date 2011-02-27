d2b9dfa62f8ab0c2630936e982e2fc41b062445b
zeyer <zeyer@688fd007-192f-0410-aed6-f784cf09bbae>
2011-02-27 12:29:33 +0000

    refactor: export Utils.ParseTree
    git-svn-id: svn+ssh://demeter.matha.rwth-aachen.de/home/Projekte/ilias/svn@3796 688fd007-192f-0410-aed6-f784cf09bbae

--- Utils.java
+++ Utils.java
@@ -4,13 +4,11 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
-import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Random;
 import java.util.Set;
@@ -648,123 +646,6 @@ public class Utils {
     	String r = "";
     	for(int i = 0; i < fac; ++i) r += s;
     	return r;
-    }
-    
-    static class ParseTree {
-    	static abstract class Entity { abstract Entity trim(); }
-    	static class Subtree extends Entity {
-    		String prefix, postfix;
-    		ParseTree content = new ParseTree();
-    		Entity trim() {
-    			Subtree t = new Subtree();
-    			t.prefix = prefix; t.postfix = postfix;
-    			t.content = content.trim();
-    			return t;
-    		}
-    	}
-    	static class RawString extends Entity {
-    		String content = "";
-    		RawString() {}
-    		RawString(String s) { content = s; }
-    		Entity trim() { return new RawString(content.trim()); }
-    	}
-    	List<Entity> entities = new LinkedList<Entity>();
-    	
-    	void parse(Iterator<Character> i, Map<String,String> bracketTypes, String stoppingBracket) {
-    		for(String s : bracketTypes.keySet()) if(s.length() != 1) throw new AssertionError("bracketsize != 1 currently not supported");
-    		for(String s : bracketTypes.values()) if(s.length() != 1) throw new AssertionError("bracketsize != 1 currently not supported");
-
-    		RawString lastRaw = null;
-    		while(i.hasNext()) {
-    			char c = i.next();
-    			if(stoppingBracket.equals("" + c)) return;
-    			if(bracketTypes.containsKey("" + c)) {
-    				lastRaw = null;
-    				Subtree subtree = new Subtree();
-    				subtree.prefix = "" + c;
-    				subtree.postfix = bracketTypes.get("" + c);
-    				entities.add(subtree);
-    				subtree.content.parse(i, bracketTypes, subtree.postfix);
-    			}
-    			else {
-    				if(lastRaw == null) {
-    					lastRaw = new RawString();
-    					entities.add(lastRaw);
-    				}
-    				lastRaw.content += c;
-    			}
-    		}
-    	}
-
-    	void parse(Iterator<Character> i, Map<String,String> bracketTypes) { parse(i, bracketTypes, ""); }
-    	void parse(String str, Map<String,String> bracketTypes) { parse(new StringIterator(str), bracketTypes); }
-    	
-    	void parse(String str, String bracketTypes) {
-    		if(bracketTypes.length() % 2 == 1) throw new AssertionError("bracketTypes string len must be a multiple of 2");
-    		Map<String,String> bracketTypesMap = new HashMap<String,String>();
-    		for(int i = 0; i < bracketTypes.length(); i += 2)
-    			bracketTypesMap.put("" + bracketTypes.charAt(i), "" + bracketTypes.charAt(i+1));
-    		parse(str, bracketTypesMap);
-		}
-    	
-    	void parse(String str) { parse(str, "()[]{}\"\""); }
-    	
-    	ParseTree() {}
-    	ParseTree(String str) { parse(str); }
-    	
-    	ParseTree trim() {
-    		ParseTree t = new ParseTree();
-    		for(Entity e : entities) t.entities.add(e.trim());
-    		return t;
-    	}
-    	void removeEmptyRawStrings() {
-    		for(Iterator<Entity> e = entities.iterator(); e.hasNext();) {
-    			RawString s = castOrNull(e.next(), RawString.class);
-    			if(s != null && s.content.isEmpty()) e.remove();
-    		}
-    	}
-    	List<ParseTree> split(Set<String> ss) {
-    		for(String s : ss) if(s.length() != 1) throw new AssertionError("str len != 1 not supported currently");
-    		List<ParseTree> l = new LinkedList<ParseTree>();
-    		{
-	    		ParseTree t = new ParseTree();
-	    		l.add(t);
-	    		for(Entity e : entities) {
-	    			if(e instanceof RawString) {
-	    				String rs = ((RawString)e).content;
-	    				RawString lastStr = new RawString();
-	    				t.entities.add(lastStr);
-	    				for(Character c : iterableString(rs)) {
-	    					if(ss.contains("" + c)) {
-	    						t.removeEmptyRawStrings();
-	            				t = new ParseTree();
-	            				l.add(t);
-	            				lastStr = new RawString();
-	            				t.entities.add(lastStr);            				
-	    					}
-	    					else
-	    						lastStr.content += c;
-	    				}
-	    			}
-	    			else
-	    				t.entities.add(e);
-	    		}
-	    		t.removeEmptyRawStrings();
-    		}
-    		// remove empty trees
-    		for(Iterator<ParseTree> i = l.iterator(); i.hasNext();) {
-    			ParseTree t = i.next();
-    			if(t.entities.isEmpty()) i.remove();
-    		}	
-    		return l;
-    	}
-    	List<ParseTree> split(String s) { Set<String> ss = new HashSet<String>(); ss.add(s); return split(ss); }
-        
-    	static ParseTree merge(List<ParseTree> tl) {
-    		ParseTree finalt = new ParseTree();
-    		for(ParseTree t : tl) finalt.entities.addAll(t.entities);
-    		return finalt;
-    	}
     }
     
     static class OperatorTree implements Comparable<OperatorTree> {
